26543bfaecee3ed31c7513da68745dc153faa305
Rafael Borges Graunke <2019306368@FARROUPILHA.IFRS.EDU.BR>
2021-12-10 11:08:24 -0300

    First test class listing

--- telaTurmaLista.php
+++ telaTurmaLista.php
@@ -5,7 +5,30 @@
     <?php include("assets/navbar.php") ?>
 
     <div class="container mt-3">
-        <div class="row"></div>
+        <div class="row">
+            <div class="col"></div>
+            <div class="col"></div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="responsive-table">
+            <table>
+                <thead>
+                    <th>Número</th>
+                    <th>Disciplina</th>
+                    <th>Período</th>
+                </thead>
+                <tbody>
+                    <?php foreach (buscaTodasTurmas() as $turma) { ?>
+                        <tr>
+                            <td><?php $turma['numero'] ?></td>
+                            <td><?php $turma['disciplina'] ?></td>
+                            <td><?php $turma['ano'] ?></td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
     </div>
 
     <?php include("includes/footer.php") ?>
