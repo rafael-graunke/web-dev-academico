13264b890f43d47ff6ec076a438e089568d9bc72
Rafael Borges Graunke <2019306368@FARROUPILHA.IFRS.EDU.BR>
2021-12-10 08:53:06 -0300

    Added includes

--- telaAlunoDetalhe.php
+++ telaAlunoDetalhe.php
@@ -4,18 +4,7 @@
 <?php $aluno = buscaAluno($id_aluno)[0]; ?>
 <?php $turmas = buscaTurmasPorAluno($_GET['id']); ?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Detalhes Aluno</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
+<?php include("includes/head.php") ?>
 
 <body>
     <?php include('assets/navbar.php') ?>
@@ -108,12 +97,9 @@
             </div>
         </div>
     </div>
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <script src="assets/js/sidebar.js"></script>
-    <script src="assets/js/modal.js"></script>
+    
+    <?php include("includes/footer.php") ?>
+    
 </body>
 
 </html>
--- telaAlunoLista.php
+++ telaAlunoLista.php
@@ -1,15 +1,10 @@
+<?php include("includes/head.php") ?>
 <?php include_once("db/alunoDao.php"); ?>
-
-<head>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
-</head>
 
 <body>
     <?php include('assets/navbar.php') ?>
     <?php include('assets/modal.php') ?>
-    <div class="container p-0 mt-3">
+    <div class="container p-0 mt-3 mb-1">
         <div class="row">
             <div class="col-auto">
                 <h3>Tabela <small class="text-muted">de alunos</small></h3>
@@ -72,8 +67,7 @@
             </tbody>
         </table>
     </div>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <script src="assets/js/modal.js"></script>
+    
+    <?php include("includes/footer.php") ?>
+    
 </body>
